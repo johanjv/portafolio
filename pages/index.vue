880deb017fac44479ce0e2625ae5281429a2d545
Johan Jose Villavicencio Valles <pnf.johanvillavicencio@gmail.com>
2024-07-11 13:04:39 -0500

    ajustes responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,51 @@
 <template>
     <div>
-        <section class="">
-            <div class="grid grid-cols-2">
+        <section class="bg-background">
+            <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 p-4">
                 <div class="px-24 py-24 items-center">
                     <span class="text-3xl font-medium">Hello 👋,</span>
                     <h1 class="text-8xl font-medium">I'am a web <br> Developer</h1>
                 </div>
-                <div class="px-24 py-14 items-center">
+                <div class="px-24 py-14 items-center ">
                     <img src="~/public/imgPage.png" alt="" srcset="">
                 </div>
             </div>
-            <div class="grid grid-cols-12 gap-2 bg-background px-2 py-2">
-                <div class="col-span-12 md:col-span-2">
+            <div class="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-12 gap-4 p-4 bg-background">
+                <div class="lg:col-span-2">
                     <div class="flex inline-block items-center justify-center align-center self-center">
                         <span class="text-7xl font-medium">5</span>
                         <span class="text-sm px-2">YEARS OF <br> EXPERIENCE</span>
                     </div>
                 </div>
-                <div class="col-span-12 md:col-span-2">
+                <div class="lg:col-span-2">
                     <div class="flex inline-block items-center justify-center align-center self-center">
                         <span class="text-7xl font-medium">+50</span>
                         <span class="text-sm px-2">PROJECTS <br> COMPLETED</span>
                     </div>
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoJS.svg" width="70" height="70"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoJS.svg" width="70" height="70" alt="" srcset="">
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoVUE.svg" width="70" height="70"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoVUE.svg" width="70" height="70" alt="" srcset="">
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoNUXT.svg" width="70" height="70"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoNUXT.svg" width="70" height="70" alt="" srcset="">
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoREACT.svg" width="70" height="70"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoREACT.svg" width="70" height="70" alt="" srcset="">
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoNativve.svg" width="40" height="40"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoNativve.svg" width="40" height="40"alt="" srcset="">
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoNext.svg" width="70" height="70"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoNext.svg" width="70" height="70" alt="" srcset="">
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoNode.svg" width="70" height="70"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoNode.svg" width="70" height="70" alt="" srcset="">
                 </div>
-                <div class="col-span-12 md:col-span-1">
-                    <div class="items-center justify-center">
-                        <img src="~/public/logoLaravel.svg" width="70" height="70"alt="" srcset="">
-                    </div>
+                <div class="justify-self-center">
+                    <img src="~/public/logoLaravel.svg" width="70" height="70" alt="" srcset="">
                 </div>
             </div>
         </section>
